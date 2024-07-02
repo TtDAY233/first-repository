13b374d6926d3a1d36fec026dff670affe1292aa
txy <t2683644759@163.com>
2024-07-02 16:30:00 +0800

    初始代码

---
+++ src/main/java/com/itheima/helloworld.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+import ch.qos.logback.classic.Logger;
+import org.w3c.dom.ls.LSOutput;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
---
+++ src/test/java/co/itheima/test01.java
@@ -0,0 +1,12 @@
+package co.itheima;
+
+import ch.qos.logback.classic.Logger;
+
+public class test01 {
+    public  void test1() {
+        System.out.println("Hello Test1");
+    }
+    public  void test2() {
+        System.out.println("Hello Test2");
+    }
+}
